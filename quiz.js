4975d49a463a77258a877f217792e47ce5fbbe30
Tony Watson <tony.watson@dna.co.nz>
2017-06-04 22:41:03 +1200

    updated again

--- quiz.js
+++ quiz.js
@@ -18,93 +18,81 @@ const allQuestions = [
     {
         question: "Who is Poo?",
         choices: ["You", "Who", "Schmoo", "Tony Blair"],
-        correctAnswer: 0
+        correctAnswer: 1
     },
     {
         question: "Who is a fish?",
         choices: ["Anus", "Bnus", "Carp", "Pnus"],
-        correctAnswer: 0
+        correctAnswer: 3
     },
     {
         question: "jadflva",
         choices: ["Yes", "No", "Yeessssssssssss", "Still not Tony Blair"],
-        correctAnswer: 0
+        correctAnswer: 2
     }
-];
 
-// function readQuestion() {
-//     //read the question options and title out of an array
-//     allQuestions.forEach(function (obj, index) {
-//         questionArray.push(allQuestions[index].question);
-//         choicesArray.push(allQuestions[index].choices);
-//         answerArray.push(allQuestions[index].correctAnswer);
-//     });
-//
-//
-//     // Do we need to be getting all the things? Can we access the required question based on index, matching the index with the question based on a click?
-// }
-
-function renderQuestion(currentQuestion) {
+function renderQuestion(quizStep) {
 //add the question text
 
     const inputName = "question[0]";
-    questionEl.innerHTML = currentQuestion.question;
+    questionEl.innerHTML = quizStep.question;
 
-    currentQuestion.choices.forEach(function(questionText, index) {
+    quizStep.choices.forEach(function(questionText, index) {
         const inputThing = `<input 
                                 type="radio" 
                                 data-index=${index} 
                                 id="item${index}" 
-                                name="${inputName}">
+                                name="${inputName}"
+                                value="${index}">
                             <label for="item${index}">${questionText}</label>
                             <br>`;
         formEl.innerHTML += inputThing;
     });
-//add the question options
-
 }
 
-//
-// function populateList(plates = [], platesList) {
-//     platesList.innerHTML = plates.map((plate, i) => {
-//             return `
-//             <li>
-//                 <input type="checkbox" data-index=${i} id="item${i}" ${plate.done ? 'checked' : ''}/>
-//                 <label for="item${i}">${plate.text}</label>
-//             </li>
-//           `;
-// }).join('');
-// }
-function correctQuestion() {
 
-    return true;
+function checkAnswer(userAnswer, quizStep) {
+    return userAnswer == quizStep.correctAnswer;
 }
 
-function nextQuestion() {
+
+function getCurrentQuizstep() {
+    return allQuestions[numberQuestion];
+}
+
+function goToNextQuestion() {
 // use a click event to progress the quiz and to assign points
     const totalQuestions = allQuestions.length;
 
     if (numberQuestion < totalQuestions ) {
-        //do the thing
         numberQuestion += 1;
     }
 }
-//show the number of question you're on
 
-function scoreCounter() {
-//for each correct answer, add 10 to the current score
-//     if (correctQuestion) {
-    score += 10;
-    // }
-
-    // update current score in span
-    scoreBoxEl.innerHTML = score;
+function getUserAnswer() {
+    return formEl.querySelector('input:checked').value;
 }
 
+//show the number of question you're on
+
+function countScore() {
+    score += 10;
+}
+
+
+function processAnswer() {
+
+    if (checkAnswer(getUserAnswer(), getCurrentQuizstep())) {
+        countScore();
+    }
+    goToNextQuestion();
+    renderQuestion(getCurrentQuizstep());
+
+}
 //detect when the last question has been answered
 
 //display final score
 
-buttonEl.addEventListener('click', scoreCounter);
+buttonEl.addEventListener('click', processAnswer);
 
-renderQuestion(allQuestions[3]);
+renderQuestion(getCurrentQuizstep());
